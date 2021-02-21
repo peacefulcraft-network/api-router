0d9beea931bb9a6736caec7f95d3b2cafa2096b3
Wyatt Parsons <wdparson@ncsa.tech>
2021-02-20 20:22:13 -1000

    Install test project dependencies too

--- scripts/start-dev-server.sh
+++ scripts/start-dev-server.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-composer --working-dir=test/ install
+composer --working-dir=test/ require
 composer --working-dir=test/ dump-autoload -o
 php -S 127.0.0.1:8081 test/src/public/test-server.php
