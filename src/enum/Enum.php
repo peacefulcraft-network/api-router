323b4ef02c02dab8a1aa9f58e1494dd27a1b5b6c
Wyatt Parsons <parsonswy@gmail.com>
2021-03-20 23:13:23 -0900

    Improve enum type wrapper

--- src/enum/Enum.php
+++ src/enum/Enum.php
@@ -2,16 +2,61 @@
 namespace ncsa\phpmvj\enum;
 
 use ReflectionClass;
+use RuntimeException;
 
 abstract class Enum {
 	private static array $_type_cache = [];
+
+	protected string|int $_value;
+	/**
+	 * Only used to access the $_value of this encapsulated enum type.
+	 * Method should not be invoked directly. See PHP __get magic method.
+	 * Access with [EnumChildObject]->_value;
+	 */
+	public function __get(string $name) {
+		if ($name !== "_value") {
+			throw new RuntimeException("Attempted to access non _value property on enumerated type.");
+		}
+		return $this->_value;
+	}
+	/**
+	 * Only used to set the $_value of this encapsulated enum type.
+	 * Method should not be invoked directly. See PHP __set magic method.
+	 * Access with [EnumChildObject]->_value = value;
+	 */
+	public function __set(string $name, mixed $value) {
+		if ($name !== "_value") {
+			throw new RuntimeException("Attempted to access non _value property on enumerated type.");
+		}
+
+		if (SELF::typeOf($value) === null) {
+			throw new RuntimeException("Supplied value failed enumerated type check.");
+		}
+
+		$this->_value = $value;
+	}
+
+	/**
+	 * Should only be called by a child class during instantiation.
+	 * @param value The value to be ecapsulated and value enforced
+	 * @throws RuntimeException When $value is not a valid representation of this type.
+	 */
+	public function __construct(string|int $value) {
+		SELF::getTypes();
+
+		if (SELF::typeOf($value) === null) {
+			throw new RuntimeException("Supplied value failed enumerated type check.");
+		}
+
+		$this->_value = $value;
+	}
 
 	/**
 	 * Takes an enumerated value and attempts to resolve it to an enumeration key.
 	 * @param value: The value to convert
 	 * @return null on failure, string or integer on success 
 	 */
-	public static function typeOf($value):string|int {
+	public static function typeOf($value):string|int|null {
 		foreach (SELF::getTypes() as $type => $enumeration) {
 			if ($value === $enumeration) { return $type; }
 		}
