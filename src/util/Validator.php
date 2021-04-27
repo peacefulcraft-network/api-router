77df928cfce918dbc07df8b214d9761c82cfcfca
Wyatt Parsons <parsonswy@gmail.com>
2021-04-26 19:30:54 -0900

    Make validator more useful

--- src/util/Validator.php
+++ src/util/Validator.php
@@ -8,7 +8,7 @@ abstract class Validator {
 	 * @param array...$values array('type'=>'exist, email, password', 'value'=>'the value'),...
 	 * @return bool true/false Values met the specified criteria
 	 */
-	public static function meetTypeRequirements(...$values): bool {
+	public static function meetTypeRequirements(array ...$values): bool {
 		foreach($values as $value) {
 			if ($value['type'] === 'exist') {
 				if (!SELF::meaningfullyExists($value['value'])) { return false; }
@@ -28,10 +28,26 @@ abstract class Validator {
 	 * @param any...$values The values to check
 	 * @return bool true/false value(s) were meaningful and exist
 	 */
-	public static function meaningfullyExists(...$values): bool {
+	public static function meaningfullyExists(string|int|float|bool|array|null ...$values): bool {
 		foreach($values as $value) {
-			if (!isset($value) || $value === null || strlen(trim($value)) === 0) {
+			// Check for general existence
+			if (!isset($value) || $value === null) {
 				return false;
+			}
+
+			// Check for significant string
+			if (is_string($value) && strlen(trim($value)) === 0) {
+				return false;
+			}
+
+			if (is_array($value)) {
+				if (count($value) === 0) {
+					return false;
+				} else {
+					foreach($value as $i) {
+						if (!SELF::meaningfullyExists($i)) { return false; }
+					}
+				}
 			}
 		}
 		return true;
@@ -42,7 +58,7 @@ abstract class Validator {
 	 * @param any...$values The values to check
 	 * @return bool true/false value(s) were emails
 	 */
-	public static function isEmail(...$values): bool {
+	public static function isEmail(?string ...$values): bool {
 		foreach($values as $value) {
 			if (!filter_var($value, FILTER_VALIDATE_EMAIL)) {
 				return false;
@@ -56,7 +72,7 @@ abstract class Validator {
 	 * @param any...$values The values to check
 	 * @return bool ture/false value(s) meet password requirements
 	 */
-	public static function couldBePassword(...$values): bool {
+	public static function couldBePassword(string|int|float|bool|null ...$values): bool {
 		foreach($values as $value) {
 			if (strlen($value) < 8) { return false; }
 		}
---
+++ test/tests/units/util/validator/ValidatorTest.php
@@ -0,0 +1,139 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use ncsa\phpmcj\util\Validator;
+
+class ValidatorTest extends TestCase {
+  public function testMeaningfullyExistsValidatorWithOneValue() {
+    $this->assertFalse(Validator::meaningfullyExists(@$undefined));
+    $this->assertFalse(Validator::meaningfullyExists(null));
+    $this->assertFalse(Validator::meaningfullyExists("  "));
+    $this->assertFalse(Validator::meaningfullyExists(""));
+    $this->assertTrue(Validator::meaningfullyExists(0));
+    $this->assertFalse(Validator::meaningfullyExists([""]));
+    $this->assertTrue(Validator::meaningfullyExists(["aasdf"]));
+    $this->assertTrue(Validator::meaningfullyExists("When it's something real, you just know."));
+  }
+
+  public function testMeaningfullyExistsValidatorWithMultipleValues() {
+    $this->assertFalse(Validator::meaningfullyExists(@$undefined, @$undefinedAgain, @$undefinedAgainAgain));
+    $this->assertFalse(Validator::meaningfullyExists(null, null, null));
+    $this->assertFalse(Validator::meaningfullyExists("  ", ""));
+    $this->assertFalse(Validator::meaningfullyExists("", "", "", []));
+	$this->assertFalse(Validator::meaningfullyExists([""], []));
+    $this->assertTrue(Validator::meaningfullyExists(["aasdf", "538ios"], ["aasdf", "538ios"]));
+    $this->assertFalse(Validator::meaningfullyExists(["aasdf", "538ios"], []));
+    $this->assertTrue(Validator::meaningfullyExists(0, 2, -1, [5, 6]));
+    $this->assertTrue(
+      Validator::meaningfullyExists(
+        "When it's something real, you just know.",
+        "Thinking something does not make it true. Wanting something does not make it real"
+      )
+    );
+  }
+
+  public function testEmailValidatorWithOneValue() {
+    $this->assertFalse(Validator::isEmail(null));
+    $this->assertFalse(Validator::isEmail(@$undefined));
+    $this->assertFalse(Validator::isEmail(''));
+    $this->assertFalse(Validator::isEmail(13));
+    $this->assertFalse(Validator::isEmail('this.is_mostly@real'));
+    $this->assertTrue(Validator::isEmail('this.is_actually@real.tld'));
+  }
+
+  public function testEmailValidatorWithMultipleValues() {
+    $this->assertFalse(Validator::isEmail(null, null, null));
+    $this->assertFalse(Validator::isEmail(@$undefined, @$undefinedAgain, @$undefined3));
+    $this->assertFalse(Validator::isEmail('', '', ''));
+    $this->assertFalse(Validator::isEmail(13, 13, 13));
+    $this->assertFalse(Validator::isEmail('this.is_mostly@real', 'this.is_mostly@real', 'this.is_mostly@real'));
+    $this->assertTrue(Validator::isEmail('this.is_actually@real.tld', 'this.is_actually@real.tld', 'this.is_actually@real.tld'));
+  }
+
+  public function testPasswordValidatorWithOneValue() {
+    $this->assertFalse(Validator::couldBePassword(null));
+    $this->assertFalse(Validator::couldBePassword(@$undefined));
+    $this->assertFalse(Validator::couldBePassword(''));
+    $this->assertFalse(Validator::couldBePassword('asdf'));
+    $this->assertFalse(Validator::couldBePassword(13));
+    $this->assertTrue(Validator::couldBePassword('th1s1s@suff1c13nt1yc0mp13xstr1ng'));
+  }
+
+  public function testPasswordValidatorWithMultipleValues() {
+    $this->assertFalse(Validator::couldBePassword(null, null, null));
+    $this->assertFalse(Validator::couldBePassword(@$undefined, @$undefinedAgain, @$undefined3));
+    $this->assertFalse(Validator::couldBePassword('', '', ''));
+    $this->assertFalse(Validator::couldBePassword('asdf', 'sdfgdsf', '35gawr'));
+    $this->assertFalse(Validator::couldBePassword(13, 17, 109503454));
+    $this->assertTrue(Validator::couldBePassword('th1s1s@suff1c13nt1yc0mp13xstr1ng', 'th1s1s@suff1c13nt1yc0mp13xstr1ng', 'th1s1s@suff1c13nt1yc0mp13xstr1ng'));
+  }
+
+  public function testMultiValidatorWithOneInvalidValueOfTypeExist() {
+    $this->assertFalse(
+      Validator::meetTypeRequirements(
+        array('type'=>'exist', 'value'=>'')
+      )
+    );
+  }
+
+  public function testMultiValidatorWithOneValidValueOfTypeExist() {
+    $this->assertTrue(
+      Validator::meetTypeRequirements(
+        array('type'=>'exist', 'value'=>'I exist!')
+      )
+    );
+  }
+
+  public function testMultiValidatorWithOneInvalidValueOfTypeEmail() {
+    $this->assertFalse(
+      Validator::meetTypeRequirements(
+        array('type'=>'email', 'value'=>'not.a_valid@email')
+      )
+    );
+  }
+
+  public function testMultiValidatorWithOneValidValueOfTypeEmail() {
+    $this->assertTrue(
+      Validator::meetTypeRequirements(
+        array('type'=>'email', 'value'=>'a_valid@email.tld')
+      )
+    );
+  }
+
+  public function testMultiValidatorWithOneInvalidValueOfTypePassword() {
+    $this->assertFalse(
+      Validator::meetTypeRequirements(
+        array('type'=>'password', 'value'=>'invalid')
+      )
+    );
+  }
+
+  public function testMultiValidatorWithOneValidValueOfTypePassword() {
+    $this->assertTrue(
+      Validator::meetTypeRequirements(
+        array('type'=>'password', 'value'=>'nowvalid!')
+      )
+    );
+  }
+
+  public function testMultiValidatorWithMultipleInvalidValuesOfVariousTypes() {
+    $this->assertFalse(
+      Validator::meetTypeRequirements(
+        array('type'=>'exist', 'value'=>''),
+        array('type'=>'email', 'value'=>''),
+        array('type'=>'email', 'value'=>'asdf'),
+        array('type'=>'password', 'value'=>'hrfidp')
+      )
+    );
+  }
+
+  public function testMultiValidatorWithMultipleValidValuesOfVariousTypes() {
+    $this->assertTrue(
+      Validator::meetTypeRequirements(
+        array('type'=>'exist', 'value'=>'I exist!'),
+        array('type'=>'email', 'value'=>'a_valid@email.tld'),
+        array('type'=>'email', 'value'=>'a_valid@email.tld'),
+        array('type'=>'password', 'value'=>'hrfidphrfidp')
+      )
+    );
+  }
+}
