935e71ffa06577d4b6ad3203d47cc63988603028
Wyatt Parsons <parsonswy@gmail.com>
2021-04-26 18:16:59 -0900

    Fix paths

--- test/scripts/run-tests.sh
+++ test/scripts/run-tests.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Get directory of script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )";
-PHPUNIT="$DIR/../vendor/bin/phpunit";
-TESTS_DIR="$DIR/../tests/units";
-BOOTSTRAP_FILE="$DIR/../tests/bootstrap.php";
-REPORT_FILE="$DIR/../../phpunit-report.xml";
+PHPUNIT="/bin/php $DIR/test/vendor/bin/phpunit";
+TESTS_DIR="$DIR/test/tests/units";
+BOOTSTRAP_FILE="$DIR/test/tests/bootstrap.php";
+REPORT_FILE="$DIR/phpunit-report.xml";
 TEST_COMMAND="$PHPUNIT $TESTS_DIR --bootstrap $BOOTSTRAP_FILE --testdox --log-junit $REPORT_FILE";
 echo $TEST_COMMAND;
 $TEST_COMMAND;
